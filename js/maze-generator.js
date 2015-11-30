f8c80497f0ba5c2b0376543fc68dff66d5483f2b
Sunjay Varma <varma.sunjay@gmail.com>
2015-11-30 02:52:14 -0500

    Wrapped asyncLoop into asyncFor for easier for loops + added delay parameter

--- js/maze-generator.js
+++ js/maze-generator.js
@@ -5,24 +5,36 @@
  */
 function generatePaths(maze) {
   var directions = Direction.all();
-  return asyncLoop(function(row, finish) {
-    if (row >= maze.rows()) {
-      return finish();
-    }
-    return asyncLoop(function(col, finish) {
-      if (col >= maze.cols()) {
-        return finish();
-      }
-
+  return asyncFor(0, maze.rows(), 1, function(row) {
+    return asyncFor(0, maze.cols(), 1, function(col) {
       var direction = directions[Math.floor(Math.random() * directions.length)];
 
       maze.openWall(row, col, direction);
-
-      return col + 1;
-    }, 0).then(function() {
-      return row + 1;
     });
-  }, 0);
+  });
+}
+
+/**
+ * Asynchronous for loop
+ * Calls callback for each item of the loop
+ * Equivalent: for (var i = start; i < end; i += step) callback(i)
+ * If callback returns a Promise, waits on that promise before continuing
+ */
+function asyncFor(start, end, step, callback, delay) {
+  return asyncLoop(function(value, finish) {
+    if (value >= end) {
+      return finish();
+    }
+
+    var result = callback(value);
+    if (!(result instanceof Promise)) {
+      result = Promise.resolve(result);
+    }
+
+    return result.then(function() {
+      return value + step;
+    });
+  }, start, delay);
 }
 
 /**
@@ -36,7 +48,7 @@ function generatePaths(maze) {
  * If the return value is a promise, that promise will be awaited
  * before continuing in the loop
  */
-function asyncLoop(callback, value) {
+function asyncLoop(callback, value, delay) {
   var done = false;
   var finish = function() {done = true;};
 
@@ -54,6 +66,6 @@ function asyncLoop(callback, value) {
           resolve(asyncLoop(callback, nextValue));
         }
       });
-    }, 0);
+    }, delay || 0);
   });
 }
