715810e4df4525719207943a4a5d416b4c48d158
Sunjay Varma <varma.sunjay@gmail.com>
2015-12-09 01:28:34 -0500

    Much better algorithm for generating solutions and backtracking when stuck

--- js/maze-generator.js
+++ js/maze-generator.js
@@ -3,47 +3,38 @@
  * 
  * Returns a promise to help determine when complete
  */
-function generatePaths(maze, minPathLength) {
-  minPathLength = minPathLength || Math.floor(Math.sqrt(maze.rows()*maze.cols()));
+function generatePaths(maze) {
+  var delay = 1200/Math.max(maze.rows(), maze.cols());
+
   var start = maze.randomEdge().setStart();
 
+  generateSolution(maze, start, delay);
+}
+
+function generateSolution(maze, start, delay) {
   var open = [start];
   var closed = new Set();
-  
-  var pathLength = 0;
-  var reachedEnd = false;
   var lastVisited = null;
-  var backwards = null;
+
   return asyncLoop(function(_, finish) {
     if (!open.length) {
-      lastVisited.removeMark();
-      return finish();
+      throw new Error("Ran out of cells before finding a solution");
     }
-    
-    if (!reachedEnd) {
-      pathLength++;
-    }
-
     var cell = open.splice(0, 1)[0].markCurrent();
-    console.log("visiting", cell);
     if (lastVisited) {
       lastVisited.removeMark();
-      if (cell.isAdjacent(lastVisited)) {
-        backwards = cell.directionTo(lastVisited);
-      }
     }
-
     lastVisited = cell;
+
+    if (closed.has(cell.id)) {
+      return;
+    }
     closed.add(cell.id);
 
-    // Ensure that the finish is not in the same row or column so the
-    // puzzle isn't too easy
-    if (!reachedEnd && !cell.isStart() && maze.isEdge(cell)
-        && start.row !== cell.row && start.col !== cell.col
-        && pathLength >= minPathLength) {
+    if (!cell.isStart() && maze.isEdge(cell)
+        && start.row !== cell.row && start.col !== cell.col) {
       cell.setFinish();
-      reachedEnd = true;
-      return;
+      return finish();
     }
 
     var adjacents = maze.adjacents(cell);
@@ -52,42 +43,55 @@ function generatePaths(maze, minPathLength) {
     });
 
     var next;
-    if (!unvisitedAdjacents.length) {
-      if (reachedEnd) {
-        // no need to continue searching
-        console.log(3);
-        return;
-      }
-
-      // in order to reach some kind of end, backtrack
-      pathLength--;
-      var openDirections = Array.from(cell.openDirections.values());
-      if (openDirections.length > 1) {
-        openDirections = openDirections.filter(function(d) {
-          return d !== backwards;
-        });
-      }
-
-      var index = Math.floor(openDirections.length * Math.random());
-      var direction = openDirections[index];
-      next = maze.adjacentTo(cell, direction);
-      console.log(1);
-      console.log(backwards);
+    if (unvisitedAdjacents.length) {
+      next = randomArrayItem(unvisitedAdjacents);
+      maze.openBetween(cell, next);
     }
     else {
-      var index = Math.floor(unvisitedAdjacents.length * Math.random());
-      next = unvisitedAdjacents[index];
-      maze.openBetween(cell, next);
-
-      Array.prototype.push.apply(open, unvisitedAdjacents.filter(function(adj) {
-        return adj !== next;
-      }));
-      console.log(2);
+      next = backtrackToUnvisited(maze, cell, closed);
     }
-    console.log("next", next);
-
     open.unshift(next);
-  }, null, 1500/Math.max(maze.rows(), maze.cols()));
+    Array.prototype.push.apply(open, unvisitedAdjacents.filter(function(adj) {
+      return adj !== next;
+    }));
+  }, null, delay);
+}
+
+function backtrackToUnvisited(maze, startCell, closed) {
+  var current = startCell;
+  var backwards = null;
+  while (current) {
+    var openDirections = Array.from(current.openDirections);
+    var openAdjacents = [];
+    for (var i = 0; i < openDirections.length; i++) {
+      var direction = openDirections[i];
+      if (openDirections.length > 1 && direction === backwards) {
+        continue;
+      }
+      var adj = maze.adjacentTo(current, direction);
+      openAdjacents.push(adj);
+    }
+
+    var next = randomArrayItem(openAdjacents);
+    backwards = next.directionTo(current);
+
+    var adjacents = maze.adjacents(next);
+    var unvisitedAdjacents = adjacents.filter(function(adj) {
+      return !closed.has(adj.id);
+    });
+    if (unvisitedAdjacents.length) {
+      var adj = randomArrayItem(unvisitedAdjacents);
+      maze.openBetween(next, adj);
+      return adj;
+    }
+
+    current = next;
+  }
+}
+
+function randomArrayItem(array) {
+  var index = Math.floor(array.length * Math.random());
+  return array[index];
 }
 
 /**
