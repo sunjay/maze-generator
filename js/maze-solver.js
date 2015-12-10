c69535cb206d1dc859e7f4744ac0a4821a5304e1
Sunjay Varma <varma.sunjay@gmail.com>
2015-12-09 21:51:12 -0500

    Added depth first search

--- js/maze-solver.js
+++ js/maze-solver.js
@@ -1,6 +1,7 @@
 var SOLVING_ALGORITHMS = {
   // these map to values in index.html
-  "breadth-first": solveMazeBreadthFirst
+  "breadth-first": solveMazeBreadthFirst,
+  "depth-first": solveMazeDepthFirst
 };
 
 function solveMaze(maze, algorithm) {
@@ -32,5 +33,33 @@ function solveMazeBreadthFirst(maze, delay) {
 
     var openAdjacents = maze.openAdjacents(current);
     Array.prototype.push.apply(open, openAdjacents);
+  }, null, delay);
+}
+
+function solveMazeDepthFirst(maze, delay) {
+  var start = maze.findStart();
+  var open = [start];
+  var visited = new Set();
+
+  return asyncLoop(function(_, finish) {
+    if (!open.length) {
+      console.error("Exhausted search.");
+      return finish();
+    }
+
+    var current = open.splice(0, 1)[0].markVisited();
+    if (visited.has(current.id)) {
+      return;
+    }
+    visited.add(current.id);
+
+    if (current.isFinish()) {
+      return finish();
+    }
+
+    var openAdjacents = maze.openAdjacents(current);
+    if (openAdjacents.length) {
+      Array.prototype.unshift.apply(open, openAdjacents);
+    }
   }, null, delay);
 }
