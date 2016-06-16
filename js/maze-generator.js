5d32dceae4989a3b0609ffe5c054fb63161e244d
Sunjay Varma <varma.sunjay@gmail.com>
2016-06-16 02:07:23 -0400

    Starting to model the problem as generating a large tree from a collected graph

--- js/maze-generator.js
+++ js/maze-generator.js
@@ -1,7 +1,7 @@
 /**
  * Generates paths through the given maze asynchronously
  * 
- * Returns a promise to help determine when complete
+ * Returns a promise to help determine when compvare
  */
 function generatePaths(maze) {
   var delay = 100/Math.max(maze.rows(), maze.cols());
@@ -9,9 +9,66 @@ function generatePaths(maze) {
   var start = maze.randomEdge().setStart();
 
   var visited = new Set();
-  return generateSolution(maze, start, visited, delay).then(function() {
-    return generateDecoys(maze, start, visited, delay);
+  return generateMazeTree(maze, start, visited, delay).then(function() {
+    return pickFinish(maze, start, delay);
   });
+}
+
+/**
+ * Generates a large tree of paths through the maze
+ */
+function generateMazeTree(maze, start, visited, delay) {
+  var open = [start];
+  var lastVisited = null;
+
+  return asyncLoop(function(_, finish) {
+    if (lastVisited) {
+      lastVisited.unmarkCurrent();
+    }
+    if (!open.length) {
+      return finish();
+    }
+    var cell = open.splice(0, 1)[0];
+
+    if (visited.has(cell.id)) {
+      return;
+    }
+    visited.add(cell.id);
+
+    var adjacents = maze.adjacents(cell);
+    var unvisitedAdjacents = adjacents.filter(function(adj) {
+      return !visited.has(adj.id);
+    });
+
+    if (unvisitedAdjacents.length) {
+      var next_index = randomArrayIndex(unvisitedAdjacents);
+      var next = unvisitedAdjacents.splice(next_index, 1)[0];
+      maze.openBetween(cell, next);
+
+      open.unshift(next);
+      Array.prototype.push.apply(open, unvisitedAdjacents);
+    }
+    // This is a way to figure out whether this is on the current
+    // path being followed or a new path caused by pushing the rest
+    // of unvisitedAdjacents onto the open array since if we were still
+    // following a path, there would be no wall between the current cell
+    // and lastVisited
+    if (lastVisited && (!lastVisited.isAdjacent(cell) || !cell.isOpenTo(lastVisited))) {
+      var adj = randomArrayItem(adjacents);
+      maze.openBetween(cell, adj);
+    }
+
+    lastVisited = cell.markGenerated().markCurrent();
+  }, null, delay);
+}
+
+/**
+ * Finds a good path and marks that as the finish
+ */
+function pickFinish(maze, start, delay) {
+  return asyncLoop(function(_, finish) {
+    return finish();
+  }, null, delay);
 }
 
 function generateSolution(maze, start, visited, delay) {
